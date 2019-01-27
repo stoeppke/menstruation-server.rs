950c9706286327f67942a224613216de54f12676
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-01-27 17:44:56 +0100

    add CORS header

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate chrono;
-extern crate menstruation;
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+extern crate chrono;
+extern crate menstruation;
 extern crate serde_json;
 
 use chrono::NaiveDate;
-use menstruation::{codes, menu};
+use menstruation::{codes, codes::Mensa, menu, menu::Meal, Response};
+use rocket_contrib::json::Json;
+use rocket::fairing::AdHoc;
+use rocket::http::Header;
 
 #[get("/menu/<mensa_code>/<date>")]
-fn menu(mensa_code: u16, date: String) -> String {
+fn menu(mensa_code: u16, date: String) -> Option<Json<Response<Meal>>> {
     let naive_date = NaiveDate::parse_from_str(&date, "%Y-%m-%d");
-    match menu::get(&mensa_code.into(), naive_date.ok()) {
-        Ok(menu_response) => format!("{}", serde_json::to_string_pretty(&menu_response).unwrap()),
-        Err(e) => e,
-    }
+    menu::get(&mensa_code.into(), naive_date.ok()).map(Json).ok()
 }
 
 #[get("/codes")]
-fn codes() -> String {
-    match codes::get() {
-        Ok(codes_response) => format!("{}", serde_json::to_string_pretty(&codes_response).unwrap()),
-        Err(e) => e,
-    }
+fn codes() -> Option<Json<Response<Mensa>>> {
+    codes::get().map(|codes_response| Json(codes_response)).ok()
 }
 
 fn main() {
     rocket::ignite()
+        .attach(AdHoc::on_response("CORS", |_, response| {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        }))
         .mount("/", routes![menu, codes])
         .launch();
 }
